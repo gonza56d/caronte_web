f0c8331059c0cb07687eedf54db7b725f678dbdc
gonza <gonza56d@gmail.com>
2021-03-08 18:40:24 -0300

    Index presentation

--- caronte/index/views.py
+++ caronte/index/views.py
@@ -4,8 +4,12 @@
 from django.shortcuts import render
 # Project
 from caronte.users.forms import SignupForm
+from caronte.utils.constants.strings import CARONTE_INDEX_DESCRIPTION
 
 
 def main(request):
     signup_form = SignupForm()
-    return render(request, 'index/main.html', {'signup_form': signup_form})
+    return render(request, 'index/main.html', {
+        'signup_form': signup_form,
+        'CARONTE_INDEX_DESCRIPTION': CARONTE_INDEX_DESCRIPTION
+    })
