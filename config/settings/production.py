fabf573efa4e31bd6ee8bfaf344aff189343d57a
gonza <gonza56d@gmail.com>
2021-04-02 03:08:59 -0300

    Separate production and development database

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,6 +7,13 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['caronte.pythonanywhere.com', 'localhost', '127.0.0.1', 'http://localhost:5500']
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': str(ROOT_DIR('production.sqlite3')),
+    }
+}
+
 # CORS_ALLOWED_ORIGINS = ['http://localhost:5500']
 
 # CORS_ORIGIN_WHITELIST = ['localhost:5500']
