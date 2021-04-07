57d01c4b8d53a57150537ac5d191d70cf7e963cb
Gonza Dambra <gonza56@gmail.com>
2021-04-07 05:01:11 +0000

    TZ fix

--- config/settings/base.py
+++ config/settings/base.py
@@ -120,7 +120,7 @@ AUTHENTICATION_BACKENDS = ['caronte.users.backends.UserBackend']
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Argentina/Buenos_Aires'
 
 USE_I18N = True
 
